0bf33cb1a812d3ba60ca1a743f893e078c956591
behtashmoradi <behtash@gmail.com>
2015-04-30 23:28:49 +1000

    add repository interface

--- HaveYourSay.Repository/ISubjectRepository.cs
+++ HaveYourSay.Repository/ISubjectRepository.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HaveYourSay.Domain.Model;
 
 namespace HaveYourSay.Repository
 {
     public interface ISubjectRepository
     {
-      
+        ISubject GetSubject(Guid subjectId);
+        IEnumerable<IObjective> GetObjectives(Guid subjectId);
     }
 }
